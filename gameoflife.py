aee87b63968ac0a53731383c48559d0c11c99f63
mae <maeyener@gmail.com>
2022-02-04 18:08:05 +0100

    Change the project structure slightly and start the pyplot debugging

--- gameoflife.py
+++ gameoflife.py
@@ -6,13 +6,16 @@
 #    By: mae <maeyener@gmail.com>                   ...   C)  A____A           #
 #                                                   :.:  ((  ( . w . )  .:.    #
 #    Created: 2022/02/03 21:31:19 by mae               .:::::::U::::U:::       #
-#    Updated: 2022/02/04 17:11:13 by mae                ..   :.: . . .:: :.    #
+#    Updated: 2022/02/04 18:07:22 by mae                ..   :.: . . .:: :.    #
 #                                                                              #
 # **************************************************************************** #
 
+import sys
+import time
 import random
 import numpy as np
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt 
+import matplotlib.animation as animation
 
 class Game:
 
@@ -22,23 +25,22 @@ class Game:
 		self.width = width
 		self.generations = generations
 		self.grid = np.zeros(width*height, dtype='int').reshape(width, height)
-		
-# We want to control the distribution ratio, so let's plug the 1's in like this
-		for i in range(0, width):
-			for j in range(0, height):
-				if random.randint(0, 100) < 15:
+
+	def init_grid(self):
+				
+		# Plug the 1's like so to control distribution ratio
+		for i in range(0, self.width):
+			for j in range(0, self.height):
+				if random.randint(0, 100) < 20:
 					self.grid[i][j] = 1
 				else:
 					self.grid[i][j] = 0
-		plt.figure()
-		plotted_img = plt.imshow(self.grid, interpolation="nearest",
-								cmap=plt.cm.gray)
-		#plt.show()
-	
+		return (self.grid)
+					
 	def count_live_neighbors(self, i, j):
 		
 		live_neighbors = 0
-		# We put "+ 2" instead of "+ 1" because the range() function is exclusive
+		# We put "+ 2" instead of "+ 1" bc range() is exclusive
 		for x in range(i - 1 , i + 2):
 			for y in range(j - 1, j + 2):
 				if x == i and y == j:
@@ -52,28 +54,37 @@ class Game:
 				else:
 					live_neighbors += self.grid[0][0]
 		
-		#print("neighbors currently alive: {}".format(live_neighbors))
-		return live_neighbors
+		return (live_neighbors)
+
+	def	update(self, args=None):
+		for i in range(0, self.width):
+			for j in range(0, self.height):
+				live = self.count_live_neighbors(i, j)
+				#print(live)
+				if self.grid[i][j] == 1 and (live > 3 or live < 2):
+					self.grid[i][j] = 0
+				elif self.grid[i][j] == 0 and live == 3:
+					self.grid[i][j] = 1
+				# Placeholder
+				else:
+					pass
+		return (self.grid)		
 
 	def run(self):
 
-		counter = 1
-		print(self.grid)
-		while counter <= self.generations:
-			for i in range(0, self.width):
-				for j in range(0, self.height):
-					live = self.count_live_neighbors(i, j)
-					#print(live)
-					if self.grid[i][j] == 1 and (live > 3 or live < 2):
-						self.grid[i][j] = 0
-					elif self.grid[i][j] == 0 and live == 3:
-						self.grid[i][j] = 1
-					# Might throw that one out, not sure it is necessary, just a placeholder for now
-					else:
-						pass
+		#fig, ax = plt.subplots( )
+		#img = ax.imshow(self.grid, interpolation='nearest' )
+		#ani = animation.FuncAnimation( fig, func=self.update, 
+		#						frames=self.generations, init_func=self.init_grid)
+		#plt.show()
+
+		i = 1
+		self.grid = self.init_grid()
+		print(self.grid, "\n\n\n")
+		while i < self.generations:
+			self.grid = self.update()
 			print(self.grid, "\n\n\n")
+			time.sleep(1)
 
-			counter += 1
-
-test_grid = Game(10, 10, 5)
-test_grid.run()
+test = Game(10, 10, 5)
+test.run()

76aba5e93d4a4246d4e8958212bfd4f90c30a9e3
mae <maeyener@gmail.com>
2022-02-10 17:57:47 +0100

    Change neighbor counting method to using 2d convolution

--- gameoflife.py
+++ gameoflife.py
@@ -6,12 +6,13 @@
 #    By: mae <maeyener@gmail.com>                   ...   C)  A____A           #
 #                                                   :.:  ((  ( . w . )  .:.    #
 #    Created: 2022/02/03 21:31:19 by mae               .:::::::U::::U:::       #
-#    Updated: 2022/02/09 23:07:34 by mae                ..   :.: . . .:: :.    #
+#    Updated: 2022/02/10 17:55:48 by mae                ..   :.: . . .:: :.    #
 #                                                                              #
 # **************************************************************************** #
 
 import numpy as np
 import pygame as pg
+from scipy import signal
 import random
 import sys
 
@@ -24,7 +25,7 @@ class GameOfLife:
 		self.w = w
 		self.h = h
 		self.s = s
-		self.grid = np.zeros((w * h), dtype='int').reshape(w, h)
+		self.grid = np.zeros((w * h), dtype="int").reshape(w, h)
 
 		# Init the base grid.
 		# Plug the 1's like so to control distribution ratio
@@ -35,42 +36,26 @@ class GameOfLife:
 				else:
 					self.grid[i][j] = 0
 
-	def count_live_neighbors(self, i, j):
+	def map_live_neighbors(self):
 
-		live_neighbors = 0
-		# Create a contextual map of the neighbors.
-		# Skip (0, 0) as it indicates current position,
-		# i.e. not a neighbor.
-		neighbor_map = [(-1, -1), (-1, 0), (-1, 1),
-						(0, -1), (0, 1),
-						(1, -1), (1, 0), (1, 1)]
+		kernel = np.ones((3 * 3), dtype="int").reshape(3, 3)
+		kernel[1, 1] = 0
+		# print (kernel, "\n", self.grid)
+		self.neighbor_map = signal.convolve2d(self.grid, kernel)[1:-1, 1:-1]
+		# print(self.neighbor_map)
 
-		# Using the remainder operation '%' allows us to find our way
-		# in that "local neighbor map" contextually.
-		for _i, _j in neighbor_map:
-			if self.grid[(i + _i) % self.w][(j + _j) % self.h]:
-				live_neighbors += 1
-		return (live_neighbors)
 
 	def update(self):
 
-		# Here, we create a second grid to edit while preserving
-		# the original one until the end of the loop.
-		new_grid = self.grid.copy()
-
+		self.map_live_neighbors()
 		for i in range(0, self.w):
 			for j in range(0, self.h):
 
-				l = self.count_live_neighbors(i, j)
-
 				# Apply Conway's rules
-				if self.grid[i][j] == 1 and (l > 3 or l < 2):
-					new_grid[i][j] = 0
-				elif self.grid[i][j] == 0 and l == 3:
-					new_grid[i][j] = 1
-
-		# The new grid replaces the precedent one.
-		self.grid = new_grid.copy()
+				if self.grid[i][j] == 1 and (self.neighbor_map[i][j] > 3 or self.neighbor_map[i][j] < 2):
+					self.grid[i][j] = 0
+				elif self.grid[i][j] == 0 and self.neighbor_map[i][j] == 3:
+					self.grid[i][j] = 1
 
 	def run(self):
 
@@ -81,7 +66,6 @@ class GameOfLife:
 		pg.display.set_caption("John Conway's Game of Life")
 
 		while True:
-			# screen.fill((41, 5, 59))
 
 			for event in pg.event.get():
 				if event.type == pg.QUIT:
@@ -89,7 +73,6 @@ class GameOfLife:
 					sys.exit()
 
 			surf = pg.surfarray.make_surface(self.grid)
-			# surf = pg.transform.scale2x(surf)
 			screen.blit(surf, (0,0))
 			pg.display.flip()
 
